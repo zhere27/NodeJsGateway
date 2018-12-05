b46c8b9ea35b32de28b23aeae84b52640f81fc29
Erwin Alcantara <zhere27@gmail.com>
2018-12-05 16:57:24 +0800

    }

--- config.js
+++ config.js
@@ -1,9 +1,12 @@
 const loading = require('lodash');
+
+// module variables
 const config = require('./config.json');
 const defaultConfig = config.development;
 const environment = process.env.NODE_ENV || 'development';
 const environmentConfig = config[environment];
 const finalConfig = loading.merge(defaultConfig, environmentConfig);
+
 global.gConfig = finalConfig;
-console.log('Configuration file loaded.');
+console.log(`global.gConfig: ${JSON.stringify(global.gConfig, undefined, global.gConfig.json_indentation)}`);
 //console.log(JSON.stringify(global.gConfig, undefined, global.gConfig.json_indentation));
